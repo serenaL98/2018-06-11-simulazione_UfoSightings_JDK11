b029eb8adc48a436bea0bdaa1d9d333ff7401225
salva <salva@LAPTOP-KKQ7JK46>
2020-06-28 22:42:26 +0200

    Scritto cose a caso

--- src/main/java/it/polito/tdp/ufo/model/Model.java
+++ src/main/java/it/polito/tdp/ufo/model/Model.java
@@ -1,5 +1,13 @@
 package it.polito.tdp.ufo.model;
 
+import it.polito.tdp.ufo.db.SightingsDAO;
+
 public class Model {
+	
+	private SightingsDAO dao;
+	
+	public Model() {
+		this.dao = new SightingsDAO();
+	}
 
 }
